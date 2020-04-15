c0bf778e52d4efea0cd7ae5cdaa6ce4edef595ef
rrm020 <rrm020@bucknell.edu>
2020-04-15 14:04:00 -0400

    script

--- HW 7/SARTools/SARTools.edgeR.transcripts.batch/Riley_script_edgeR.r
+++ HW 7/SARTools/SARTools.edgeR.transcripts.batch/Riley_script_edgeR.r
@@ -20,8 +20,8 @@ if (!require("SARTools")) install_github("KField-Bucknell/SARTools", build_vigne
 ################################################################################
 rm(list=ls())                                        # remove all the objects from the R session
 
-workDir <- "/Users/rileymcdonnell/Desktop/Biology 364/rrm020/HW 7/SARTools/SARTools.edgeR.transcripts"
-projectName <- "SARTools.edgeR.transcripts"                         # name of the project
+workDir <- "/Users/rileymcdonnell/Desktop/Biology 364/rrm020/HW 7/SARTools/SARTools.edgeR.transcripts.batch"
+projectName <- "SARTools.edgeR.transcripts.batch"                         # name of the project
 author <- "Riley McDonnell"                                # author of the statistical analysis/report
 
 targetFile <- "../transcripts.target.txt"                           # path to the design/target file
@@ -30,7 +30,7 @@ featuresToRemove <- NULL
 
 varInt <- "Treatment"                                    # factor of interest
 condRef <- "Untreated"                                      # reference biological condition
-batch <- NULL                                        # blocking factor: NULL (default) or "batch" for example
+batch <- "batch"                                        # blocking factor: NULL (default) or "batch" for example
 
 idColumn = 1                                         # column with feature Ids (usually 1)
 countColumn = 5                                      # column with counts  (2 for htseq-count, 7 for featurecounts, 5 for RSEM/Salmon, 4 for kallisto)
