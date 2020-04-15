6842c24cad93ce343702c05a589752636db1f53f
rrm020 <rrm020@bucknell.edu>
2020-04-15 14:32:00 -0400

    update this file

--- HW 7/DEBrowser/DEBrowser.R
+++ HW 7/DEBrowser/DEBrowser.R
@@ -2,8 +2,7 @@
 # 1. Install DEBrowser and its dependencies by running the lines below
 # in R or RStudio.
 
-if (!requireNamespace("BiocManager", quietly=TRUE))
-  install.packages("BiocManager")
+if (!requireNamespace("BiocManager", quietly=TRUE)) install.packages("BiocManager")
 BiocManager::install("debrowser")
 
 # 2. Load the library
